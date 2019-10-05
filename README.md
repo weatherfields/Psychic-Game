962fa1226e0811628c6b27f92b686ff36fa14ce7
Chris Cuomo <weatherfields@protonmail.com>
2019-10-05 15:05:44 -0400

    final push for everything

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Psychic-Game
+Thank you for playing The Psychic Game!
+# Instructions:
+Your objective is to psychically guess which letter the computer is thinking.
+- To begin, select any key from a to z to start the game and make your first guess.
+- You have 10 guesses before you either win or lose.
+- the game will restart after a win or a loss.
+- The game will automatically log your wins and losses.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,91 +1,107 @@
+// I left all of my unused/replaced code at the bottom. Please let me know if this is helpful, or if I should just delete it next time. Thank you!
+
+// global vars
+
 let wins = 0;
 let losses = 0;
 let guessesLeft = 10;
 let guessesSoFar = [];
-const goodGuesses = [];
-const badGuesses = [];
-var userGuess = "";
+let userGuess = "";
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var randomNumber = Math.floor(Math.random() * 26)
+// HTML element variables
 
+let winsElement = document.getElementById("wins");
+let losesElement = document.getElementById("losses");
+let guessesLeftElement = document.getElementById("guesses-left");
+let userGuessElement = document.getElementById("guesses-so-far");
 
+// Set initial value for HTML variables
 
-const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-];
+winsElement.innerHTML = wins;
+guessesLeftElement.innerHTML = guessesLeft;
 
-var compGuess = alphabet[randomNumber];
-console.log(compGuess);
+alert("Welcome to The Psychic Game, the game where it's your job to psychically guess what letter the computer is thinking! Please select any key from a to z to start the game and make your first guess.");
 
+//assign initial values to elements
 
-function testGuess() {
-    console.log(userGuess);
-    //if guessed correctly,
+document.onkeyup = function (event) {
+    userGuess = event.key.toLowerCase();
+    let compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+    // console.log(compGuess);
 
     if (userGuess === compGuess) {
         wins++;
-        goodGuesses.push(userGuess);
+        guessesSoFar = [];
+        guessesLeft = 10;
         alert("good guess! you have " + wins + " wins");
-        document.getElementById("wins").innerHTML = wins;
-        console.log(badGuesses)
-
-    } else if (userGuess !== compGuess) {
-
+        // I didn't really understand this until reading ... https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_innerhtml
+        winsElement.innerHTML = wins;
+        guessesLeftElement.innerHTML = guessesLeft;
+        userGuessElement.innerHTML = "";
+    } 
+    else if (userGuess != compGuess) {
+        guessesLeft--;
+        // https://www.w3schools.com/jsref/jsref_push.asp
+        guessesSoFar.push(userGuess);
+        guessesLeftElement.textContent = guessesLeft;
+        // I learned about the .join method after googling how to make an array visible as I'm typing it (which is converting it to a string). https://www.w3schools.com/jsref/jsref_join.asp
+        userGuessElement.innerHTML = guessesSoFar.join(" ");
+    } 
+    if (guessesLeft === 0) {
         losses++;
-        badGuesses.push(userGuess);
-        document.getElementById("losses").innerHTML = losses;
-        console.log(badGuesses)
-    } else {
-        alert("no key was selected!");
+        guessesLeft = 10;
+        guessesSoFar = [];
+        alert("Sorry, you ran out of guesses! You have " + losses + " losses");
+        losesElement.innerHTML = losses;
+        guessesLeftElement.textContent = guessesLeft;
+        userGuessElement.innerHTML = "";
     }
-
-
 }
+    // else {
+    //     guessesLeft--;
+    //     guessesSoFar.push(userGuess);
+    //     document.getElementById("guesses-so-far").innerHTML = guessesSoFar.join(" "); }
+        
+        //badGuesses.push(userGuess);
+        // user failed
+        // guessesLeft decreases
+        // we push guessed letter to guessesSoFar array 
+        // possibility of losing    
+        //display values
+        // if loses, 
+        // losses increase
+        //game resets and we start again
+        // what happens when game resets?
 
-document.onkeyup = function (event) {
-
-    userGuess = event.key;
-
-    testGuess();
-
-
-}
-
-
-
-console.log(userGuess);
-
-document.getElementById("wins").innerHTML = wins;
-document.getElementById("losses").innerHTML = losses;
-document.getElementById("guesses-left").innerHTML = guessesLeft;
-// Learned about .join here: https://www.w3schools.com/jsref/jsref_join.asp
-document.getElementById("guesses-so-far").innerHTML = guessesSoFar.join(" ");
-
-
-// the app randomly picks a letter, and 
-
-
-//the user has to guess which letter the app chose. 
-
+        // losses++;
+        // badGuesses.push(userGuess);
+        // document.getElementById("losses").innerHTML = losses;
+        // console.log(badGuesses)
+    
+    // console.log(userGuess)
+    // testGuess();
+    // 
 
 
+// guessesSoFar = [];
+        // guessesSoFar = [];
+        // guessesLeft--;
+        // document.getElementById("guesses-so-far").innerHTML = guessesSoFar.join(" ");
+        
+        // reset so taht coputer guessess again.
+        // User wins, 
+        // increment wins by one, reset game (what does resetting do?)...
+        // keep playing, computer guesses again
+        // display values, which includes values of guessesSoFAr
+        // goodGuesses.push(userGuess);
 
-// Guess what letter I'm thinking of -comparison btw user guess & comp guess
+        //add guessed value to array, use guessesSoFar.push("actual Value")
+        // console.log(badGuesses)
 
-
-// Wins: (# of times the user has guessed the letter correctly)
-
-
-// Losses: (# of times the user has failed to guess the letter correctly ***after exhausting all guesses***)
-
-
-// Guesses Left: (# of guesses left. ****This will update*****)
-
-
-// Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
-
-
-// When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
-
-
-// When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
+        // let goodGuesses = [];
+// let badGuesses = [];
+// var compGuess = alphabet[randomNumber];
+// let guessesLeftP = document.getElementById('guesses-left');
+// geussesLeftP.textContent = guessesLeft;
